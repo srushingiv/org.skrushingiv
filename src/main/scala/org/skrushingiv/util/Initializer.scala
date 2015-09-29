39407544d9b81b2d1b9bec17a6b40ecf58b24043
Sam Rushing <skrushing@intelligent-es.com>
2015-09-29 12:00:46 -0400

    Adding non-sugar syntax for initializer

--- src/main/scala/org/skrushingiv/util/Initializer.scala
+++ src/main/scala/org/skrushingiv/util/Initializer.scala
@@ -11,7 +11,7 @@ package org.skrushingiv.util
  *     )
  *     import myInitializer._
  * 
- *     lazy val "foo service" is new FooService(...)
+ *     lazy val fooService = "foo service" is new FooService(...)
  * 
  * And when a specific post-initialization side-effect is required that handler function can be
  * defined as an implicit.
@@ -21,18 +21,29 @@ package org.skrushingiv.util
  *       log(s"initialized $name as Bar with $someCalc")
  *     }
  *     
- *     lazy val "bar" is new Bar(...)
+ *     lazy val bar = "bar" is new Bar(...)
+ * 
+ * Alternate syntax without sugar:
+ * 
+ *     val Init = Initializer(
+ *       before = (name:String) => log(s"initializing $name),
+ *       after = (name:String, value:Any) => log(s"initialized $name as $value")
+ *     )
+ *     
+ *     lazy val fooService = Init("fooService") { new FooService(...) }
  * 
  */
 
-class Initializer(before:Initializer.BeforeInitializer, defaultAfter:Initializer.AfterInitializer[Any]) {
+class Initializer(before:Initializer.BeforeInitializer, defaultAfter:Initializer.AfterInitializer[Any]) { self =>
   import Initializer._
 
+  def apply[A](name:String)(value: => A)(implicit initialized: AfterInitializer[A] = defaultAfter) = {
+    before(name)
+    value and (initialized(name,_))
+  }
+
   implicit class InitializerSugar(name:String) {
-    def is[A](value: => A)(implicit initialized: AfterInitializer[A] = defaultAfter) : A = {
-      before(name)
-      value and (initialized(name,_))
-    }
+    def is[A](value: => A)(implicit initialized: AfterInitializer[A] = defaultAfter) : A = self(name)(value)
   }
 }
 
--- src/test/scala/org/skrushingiv/util/InitializerSpec.scala
+++ src/test/scala/org/skrushingiv/util/InitializerSpec.scala
@@ -3,7 +3,7 @@ package org.skrushingiv.util
 import org.specs2.mutable._
 import org.specs2.specification.Scope
 
-class InitializationSpec extends Specification {
+class InitializerSpec extends Specification {
 
   trait Fixture extends Scope {
     var x:Option[String] = None
@@ -14,7 +14,7 @@ class InitializationSpec extends Specification {
     )
   }
 
-  "initializing" should {
+  "initializing with sugar" should {
 
     "execute the default side-effects when no implicits are available" in new Fixture {
       import TestInitializer._
@@ -40,6 +40,33 @@ class InitializationSpec extends Specification {
       z2 === "456"
       x === Some("def")
       y === Some("def" -> "456")
+    }
+  }
+
+  "initializing without sugar" should {
+
+    "execute the default side-effects when no implicits are available" in new Fixture {
+      val z = TestInitializer("abc") { 123 }
+      
+      x === Some("abc")
+      y === Some("abc" -> 123)
+      z === 123
+    }
+
+    "execute the implicit side effects when available" in new Fixture {
+      implicit def afterIntInitializer(name:String,value:String) = {
+        y = Some( name -> ("test",value))
+      }
+      
+      val z1 = TestInitializer("abc") { 123 }
+      lazy val z2 = TestInitializer("def") { "456" }
+      
+      x === Some("abc")
+      y === Some("abc" -> 123)
+      z1 === 123
+      z2 === "456"
+      x === Some("def")
+      y === Some("def" -> ("test","456"))
     }
   }
 
